411670cdca8619e135297f4cca5cec2d457c64f9
Yoshikrit <63050105@kmitl.ac.th>
2022-12-10 23:04:55 +0700

    first commit

--- src/weekcovidsoapclient/WeekCovidSoapClient.java
+++ src/weekcovidsoapclient/WeekCovidSoapClient.java
@@ -61,48 +61,53 @@ public class WeekCovidSoapClient {
             
             System.out.println(jsona);
             
-            JsonObject firstname = jsona.getJsonObject(0);
+            for(int i = 0 ; i < jsona.size() ; i++){
+                JsonObject firstname = jsona.getJsonObject(i);
             
-            int year = firstname.getInt("year");
+                int year = firstname.getInt("year");
+
+                int weeknum = firstname.getInt("weeknum");
+
+                int new_case = firstname.getInt("new_case");
+
+                int total_case = firstname.getInt("total_case");
+
+                int new_case_excludeabroad = firstname.getInt("new_case_excludeabroad");
+
+                int total_case_excludeabroad = firstname.getInt("total_case_excludeabroad");
+
+                int new_recovered = firstname.getInt("new_recovered");
+
+                int total_recovered = firstname.getInt("total_recovered");
+
+                int new_death = firstname.getInt("new_death");
+
+                int total_death = firstname.getInt("total_death");
+
+                int case_foreign  = firstname.getInt("case_foreign");
+
+                int case_prison = firstname.getInt("case_prison");
+
+                int case_walkin = firstname.getInt("case_walkin");
+
+                int case_new_prev = firstname.getInt("case_new_prev");
+
+                int case_new_diff = firstname.getInt("case_new_diff");
+
+                int death_new_prev = firstname.getInt("death_new_prev");
+
+                int death_new_diff = firstname.getInt("death_new_diff");
+
+                String update_date = firstname.getString("update_date");
+                SimpleDateFormat DateFor = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                Date date = DateFor.parse(update_date);
+                
+                addDatabase(year,weeknum,new_case,total_case,new_case_excludeabroad,total_case_excludeabroad,new_recovered,total_recovered,new_death,total_death,case_foreign,case_prison,case_walkin,case_new_prev,case_new_diff,death_new_prev,death_new_diff,date);
+        
+            }
             
-            int weeknum = firstname.getInt("weeknum");
-            
-            int new_case = firstname.getInt("new_case");
-            
-            int total_case = firstname.getInt("total_case");
-            
-            int new_case_excludeabroad = firstname.getInt("new_case_excludeabroad");
-            
-            int total_case_excludeabroad = firstname.getInt("total_case_excludeabroad");
-            
-            int new_recovered = firstname.getInt("new_recovered");
-            
-            int total_recovered = firstname.getInt("total_recovered");
-            
-            int new_death = firstname.getInt("new_death");
-            
-            int total_death = firstname.getInt("total_death");
-            
-            int case_foreign  = firstname.getInt("case_foreign");
-            
-            int case_prison = firstname.getInt("case_prison");
-            
-            int case_walkin = firstname.getInt("case_walkin");
-            
-            int case_new_prev = firstname.getInt("case_new_prev");
-            
-            int case_new_diff = firstname.getInt("case_new_diff");
-            
-            int death_new_prev = firstname.getInt("death_new_prev");
-            
-            int death_new_diff = firstname.getInt("death_new_diff");
-            
-            String update_date = firstname.getString("update_date");
-            SimpleDateFormat DateFor = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-            Date date = DateFor.parse(update_date);
-            
-            addDatabase(year,weeknum,new_case,total_case,new_case_excludeabroad,total_case_excludeabroad,new_recovered,total_recovered,new_death,total_death,case_foreign,case_prison,case_walkin,case_new_prev,case_new_diff,death_new_prev,death_new_diff,date);
-        } else {
+        } 
+        else {
             System.out.println("GET request did not work.");
         }
     }
